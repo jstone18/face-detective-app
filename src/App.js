b2dee4b18d6276bc7dc59fd8ce150c08f700778d
jstone18 <jason.stone0808@gmail.com>
2019-12-31 14:45:36 -0500

    Create image form and add styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Logo from "./components/logo/Logo";
+import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
 			<Logo />
+			<ImageLinkForm />
 		</div>
 	);
 }
