ba59e2f8f0a8d663c96673826acbbd6d9badcd84
jstone18 <jason.stone0808@gmail.com>
2019-12-31 17:33:18 -0500

    Add Particles background to App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Logo from "./components/logo/Logo";
 import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
+import Rank from "./components/rank/Rank";
+import Particles from "react-particles-js";
+
+const particlesOptions = {
+	particles: {
+		number: {
+			value: 30,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		}
+	}
+};
 
 function App() {
 	return (
 		<div className="App">
+			<Particles className="particles" params={particlesOptions} />
 			<Navbar />
 			<Logo />
+			<Rank />
 			<ImageLinkForm />
 		</div>
 	);
