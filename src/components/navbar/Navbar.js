02996e4ba5336820a43e0eda06c3db3c5b3393da
jstone18 <jason.stone0808@gmail.com>
2019-12-31 13:46:02 -0500

    Create navbar component with simple signout link

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<div>
+			<nav style={{ display: "flex", justifyContent: "flex-end" }}>
+				<p className="f3 link dim black underline pa3 pointer">SignOut</p>
+			</nav>
+		</div>
+	);
+};
+
+export default Navbar;
