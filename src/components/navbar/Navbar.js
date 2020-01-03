ee1e138d27e0fdc6b79767b67753c690ae2cd500
jstone18 <jason.stone0808@gmail.com>
2020-01-03 18:03:33 -0500

    Update navbar with conditional statement to display nav links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,32 @@
 import React from "react";
 
-const Navbar = () => {
-	return (
-		<div>
+const Navbar = ({ onRouteChange, signedIn }) => {
+	if (signedIn) {
+		return (
 			<nav style={{ display: "flex", justifyContent: "flex-end" }}>
-				<p className="f3 link dim black underline pa3 pointer">SignOut</p>
+				<p
+					onClick={() => onRouteChange("signout")}
+					className="f3 link dim black underline pa3 pointer">
+					SignOut
+				</p>
 			</nav>
-		</div>
-	);
+		);
+	} else {
+		return (
+			<nav style={{ display: "flex", justifyContent: "flex-end" }}>
+				<p
+					onClick={() => onRouteChange("signin")}
+					className="f3 link dim black underline pa3 pointer">
+					Sign In
+				</p>
+				<p
+					onClick={() => onRouteChange("register")}
+					className="f3 link dim black underline pa3 pointer">
+					Register
+				</p>
+			</nav>
+		);
+	}
 };
 
 export default Navbar;
