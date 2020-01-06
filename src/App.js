0f663199e1c14b5efbcb9e25940fc11efc1152d0
jstone18 <jason.stone0808@gmail.com>
2020-01-06 02:01:16 -0500

    Upd App.js w/ fetch req.  to increase entry count.

--- src/App.js
+++ src/App.js
@@ -79,13 +79,25 @@ class App extends Component {
 		this.setState({ input: e.target.value });
 	};
 
-	onButtonSubmit = () => {
+	onImageSubmit = () => {
 		this.setState({ imageUrl: this.state.input });
 		app.models
 			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-			.then(response =>
-				this.displayFacialSquare(this.calculateFaceLocation(response))
-			)
+			.then(response => {
+				if (response) {
+					fetch("http://localhost:3000/image", {
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({ id: this.state.user.id })
+					})
+						.then(response => response.json())
+						.then(count => {
+							this.setState(Object.assign(this.state.user, { entries: count }));
+						});
+				}
+
+				this.displayFacialSquare(this.calculateFaceLocation(response));
+			})
 			.catch(err => console.log(err));
 	};
 
@@ -107,15 +119,18 @@ class App extends Component {
 				{route === "home" ? (
 					<div>
 						<Logo />
-						<Rank />
+						<Rank
+							name={this.state.user.name}
+							entries={this.state.user.entries}
+						/>
 						<ImageLinkForm
 							onInputChange={this.onInputChange}
-							onButtonSubmit={this.onButtonSubmit}
+							onImageSubmit={this.onImageSubmit}
 						/>
 						<FaceRecognition box={box} imageUrl={imageUrl} />
 					</div>
 				) : route === "signin" ? (
-					<Signin onRouteChange={this.onRouteChange} />
+					<Signin onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
 				) : (
 					<Register
 						onRouteChange={this.onRouteChange}
